43fc0b771528cf3e87a59de54a218223242963b9
Alleyne-Angel <alleynecheng@outlook.com>
2024-05-22 09:38:44 +0800

    Initial finished codes
    Web with introduction, news, gacha about Hidden is Fun.
    Cards in gacha still need modifying, like unifying the size and structures (including stars that it is) of cards.

---
+++ static/js/cards.js
@@ -0,0 +1,86 @@
+const cards = {
+  cards: [
+    [
+      {
+        path: "static/assets/gacha/1/0.png",
+        star: 1,
+        event: null,
+        available: true,
+      },
+      {
+        path: "static/assets/gacha/1/1.png",
+        star: 1,
+        event: null,
+        available: true,
+      },
+    ],
+    [
+      {
+        path: "static/assets/gacha/2/0.png",
+        star: 2,
+        event: null,
+        available: true,
+      },
+    ],
+    [
+      {
+        path: "static/assets/gacha/3/0.png",
+        star: 3,
+        event: null,
+        available: true,
+      },
+      {
+        path: "static/assets/gacha/3/1.jpg",
+        star: 3,
+        event: null,
+        available: true,
+      },
+    ],
+    [
+      {
+        path: "static/assets/gacha/4/0-1.png",
+        star: 4,
+        event: null,
+        available: true,
+      },
+      {
+        path: "static/assets/gacha/4/0.jpg",
+        star: 4,
+        event: null,
+        available: true,
+      },
+      {
+        path: "static/assets/gacha/4/1-1.png",
+        star: 4,
+        event: null,
+        available: true,
+      },
+      {
+        path: "static/assets/gacha/4/1.jpg",
+        star: 4,
+        event: null,
+        available: true,
+      },
+    ],
+    [
+      {
+        path: "static/assets/gacha/5/0.jpg",
+        star: 5,
+        event: null,
+        available: true,
+      },
+      {
+        path: "static/assets/gacha/5/1.jpg",
+        star: 5,
+        event: null,
+        available: true,
+      },
+      {
+        path: "static/assets/gacha/5/2.jpg",
+        star: 5,
+        event: null,
+        available: true,
+      },
+    ],
+  ],
+};
